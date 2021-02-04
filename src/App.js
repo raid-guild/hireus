1acdd039fe66b8babb343ae6ea311ff5119300b3
Manolingam N <manolingam1996@gmail.com>
2021-02-04 12:17:17 +0530

    some more UI tweaks

--- src/App.js
+++ src/App.js
@@ -30,71 +30,73 @@ const App = () => {
 
   return (
     <div className='app'>
-      {windowWidth < 1000 && (
-        <div className='window'>
-          <motion.h1
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.6, duration: 0.5 }}
-          >
-            Hiring RaidGuild
-          </motion.h1>
-          <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.7, duration: 0.5 }}
-          >
-            Please use your desktop or resize your window to more than 1000px to
-            proceed.
-          </motion.p>
-        </div>
-      )}
-      {windowWidth > 1000 && (
-        <>
-          <motion.img
-            id='raidguild-logo'
-            src={raidguild__logo}
-            alt='raidguild logo'
-            initial={{ y: -250 }}
-            animate={{ y: -10 }}
-            transition={{ delay: 0.3 }}
-          />
-          <Router>
-            <Switch>
-              <Route path='/' exact>
-                <>
-                  {context.stage === 1 && <HeadsUp />}
-                  {context.stage === 2 && <PersonalInfo />}
-                  {context.stage === 3 && <ProjectInfo />}
-                  {context.stage === 4 && <RequiredServices />}
-                  {context.stage === 5 && <AdditionalInfo />}
-                  {context.stage === 6 && <Confirmation />}
-                  {context.stage === 7 && <Feedback />}
+      <div className='main'>
+        {windowWidth < 1000 && (
+          <div className='window'>
+            <motion.h1
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.6, duration: 0.5 }}
+            >
+              Hiring RaidGuild
+            </motion.h1>
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.7, duration: 0.5 }}
+            >
+              Please use your desktop or resize your window to more than 1000px
+              to proceed.
+            </motion.p>
+          </div>
+        )}
+        {windowWidth > 1000 && (
+          <>
+            <motion.img
+              id='raidguild-logo'
+              src={raidguild__logo}
+              alt='raidguild logo'
+              initial={{ y: -250 }}
+              animate={{ y: -10 }}
+              transition={{ delay: 0.3 }}
+            />
+            <Router>
+              <Switch>
+                <Route path='/' exact>
+                  <>
+                    {context.stage === 1 && <HeadsUp />}
+                    {context.stage === 2 && <PersonalInfo />}
+                    {context.stage === 3 && <ProjectInfo />}
+                    {context.stage === 4 && <RequiredServices />}
+                    {context.stage === 5 && <AdditionalInfo />}
+                    {context.stage === 6 && <Confirmation />}
+                    {context.stage === 7 && <Feedback />}
 
-                  {context.stage !== 1 && context.stage !== 6 && (
-                    <button
-                      id='prev-stage-button'
-                      onClick={() => context.updateStage('previous')}
-                    >
-                      <i className='fas fa-arrow-left'></i>
-                    </button>
-                  )}
+                    {context.stage !== 1 && context.stage !== 6 && (
+                      <button
+                        id='prev-stage-button'
+                        onClick={() => context.updateStage('previous')}
+                      >
+                        <i className='fas fa-arrow-left'></i>
+                      </button>
+                    )}
 
-                  {context.stage !== 1 && (
-                    <button
-                      onClick={() => context.updateFaqModalStatus(true)}
-                      id='faq-button'
-                    >
-                      Read FAQ
-                    </button>
-                  )}
-                </>
-              </Route>
-            </Switch>
-          </Router>
-          <FAQ />
-        </>
-      )}
+                    {context.stage !== 1 && (
+                      <button
+                        onClick={() => context.updateFaqModalStatus(true)}
+                        id='faq-button'
+                      >
+                        Read FAQ
+                      </button>
+                    )}
+                  </>
+                </Route>
+              </Switch>
+            </Router>
+            <FAQ />
+          </>
+        )}
+      </div>
     </div>
   );
 };
