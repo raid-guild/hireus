3e2ce427862166c571d7ebc02b65bd209a9ff9d8
ECWireless <Elliott@valtechcreative.com>
2021-07-08 16:00:49 -0600

    Update HeadsUp copy

--- src/stages/HeadsUp.jsx
+++ src/stages/HeadsUp.jsx
@@ -33,12 +33,10 @@ const HeadsUp = () => {
           animate={{ opacity: 1 }}
           transition={{ delay: 0.6, duration: 0.5 }}
         >
-          To hire RaidGuild, the first step is to book a paid consultation. The
-          consultation costs 500 DAI and requires a web3 enabled browser. After
-          receiving your request, our raiders will study any info, specs, docs
-          and assets sent along with your proposal. A Cleric of the Guild will
-          reach out to confirm a time and date for the first call within 72
-          hours.
+          To hire RaidGuild, you must first request a consultation. 
+          Consultations have a base cost of $500 xDai, and are initially 
+          ordered by date (ascending). Anyone can add a bounty (in $RAID) 
+          to an open consultation.
         </motion.p>
 
         <motion.p
@@ -46,12 +44,10 @@ const HeadsUp = () => {
           animate={{ opacity: 1 }}
           transition={{ delay: 0.7, duration: 0.5 }}
         >
-          In most cases, we use this time (and some time after) to understand
-          your needs and deliver a comprehensive statement of work, ensuring the
-          key points and timeline are locked in before we begin. In all cases,
-          we strive to deeply understand how RaidGuild — with all the expertise
-          available — can better help you solve problems and bring your visions
-          into reality.
+          Raid Guild will prioritize the highest bounties when time and 
+          specialists become available for new consultations. In times of 
+          high demand, this might mean consultations with low or no bounty 
+          have a longer turnaround.
         </motion.p>
 
         <motion.p
@@ -59,11 +55,10 @@ const HeadsUp = () => {
           animate={{ opacity: 1 }}
           transition={{ delay: 0.8, duration: 0.5 }}
         >
-          For more info about the consultation, value and process, check our{' '}
+          For more info about the consultation check our{' '}
           <button id='faq' onClick={() => context.updateFaqModalStatus(true)}>
-            FAQ.
-          </button>{' '}
-          We look forward to raiding with you.
+            docs
+          </button>{'.'}
         </motion.p>
         <motion.a
           id='alchemy-badge'
