71dad9f342f0e9477e37b82a46cf107d757312bb
ECWireless <Elliott@valtechcreative.com>
2021-07-08 17:30:58 -0600

    Add bounty card

--- src/stages/HiringBoard.jsx
+++ src/stages/HiringBoard.jsx
@@ -3,6 +3,34 @@ import { motion } from 'framer-motion';
 
 import { AppContext } from '../context/AppContext';
 
+const dummyData = [
+  {
+    date: '2021-06-11',
+    name: 'Alice project',
+    amount: 500
+  },
+  {
+    date: '2021-06-17',
+    name: 'Charlie project',
+    amount: 450
+  },
+  {
+    date: '2021-06-16',
+    name: 'Bob project',
+    amount: 100
+  },
+  {
+    date: '2021-06-17',
+    name: 'Ellie project',
+    amount: 1
+  },
+  {
+    date: '2021-06-17',
+    name: 'Dan project',
+    amount: 0
+  }
+];
+
 const HiringBoard = () => {
   const context = useContext(AppContext);
 
@@ -27,7 +55,7 @@ const HiringBoard = () => {
             ordered by date (ascending). Anyone can add a bounty (in $RAID) 
             to an open consultation.
           </motion.p>
-          <div>
+          <div id='consultation-button-container'>
             <button
               id='new-consultation-button'
               initial={{ x: '100vw' }}
@@ -53,7 +81,27 @@ const HiringBoard = () => {
           </div>
         </div>
         <div className="hiringboard-card">
-          Right card.
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.7, duration: 0.5 }}
+          >
+            Highest bounties:
+          </motion.h2>
+          <div className="bounty-list">
+            {dummyData.map((item, index) => (
+              <div key={index} className={`bounty-list-item bounty-list-item${index % 2 !== 0 && '--2'}`}>
+                <div className="bounty-list-item-inner">
+                  <p>{item.date}</p>
+                  <p>{item.name}</p>
+                </div>
+                <div className="bounty-list-item-inner">
+                  <p>{item.amount} $RAID</p>
+                  <button>open</button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
