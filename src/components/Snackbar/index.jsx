7ffa5e0ec9d56f915ec24be51d4f505013ee9994
ECWireless <Elliott@valtechcreative.com>
2021-07-16 10:24:52 -0600

    Update snackbar styles

--- src/components/Snackbar/index.jsx
+++ src/components/Snackbar/index.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './snackbar.scss'
+import { ReactComponent as EtherscanSvg } from '../../assets/etherscan.svg';
 
 export default function Snackbar(props) {
   return (
     <div id="snackbar-position">
         <div id="snackbar-container">
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            {props.message && <p id="snackbar-status">{props.message}</p>}
-            {props.hash && <a
-              id="snackbar-link"
-              target="_blank"
-              rel="noreferrer"
-              href={`https://rinkeby.etherscan.io/tx/${props.hash}`}
+          <motion.a
+            href={`https://rinkeby.etherscan.io/tx/${props.hash}`}
+            target={'_blank'}
+            rel={'noopener noreferrer'}
+            id="snackbar-link"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6, duration: 0.5 }}
+          >
+            <EtherscanSvg />
+          </motion.a>
+          {props.message && (
+            <motion.p
+              id="snackbar-status"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.6, duration: 0.5 }}
             >
-              View status
-            </a>}
-          </div>
+              {props.message}
+            </motion.p>
+          )}
           <div id="snackbar-close-container" onClick={() => props.setShowSnackbar(false)}>
             <div id="close-icon">&#10005;</div>
           </div>
