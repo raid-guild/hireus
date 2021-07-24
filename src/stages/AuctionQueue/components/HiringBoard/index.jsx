fef56f8188b7f2cd30e96982d5fe88086a2d1199
ECWireless <Elliott@valtechcreative.com>
2021-07-23 19:27:35 -0600

    Update hiring board description

--- src/stages/AuctionQueue/components/HiringBoard/index.jsx
+++ src/stages/AuctionQueue/components/HiringBoard/index.jsx
@@ -13,64 +13,116 @@ export const HiringBoard = ({
 
   return (
     <div className="hiringboard-container">
-      <motion.h1
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.5, duration: 0.5 }}
-      >
-        Hiring Board
-      </motion.h1>
       <div className="hiringboard-card-container">
-        <div className="hiringboard-card">
+        <div className="hiringboard-description-container">
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+          >
+            Hiring Raid Guild
+          </motion.h1>
           <motion.p
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.6, duration: 0.5 }}
           >
-            To hire RaidGuild, you must first request a consultation. 
-            Consultations have a base cost of $500 xDai, and are initially 
-            ordered by date (ascending). Anyone can add a bid (in $RAID) 
-            to an open consultation.
+            Every consultation has a base fee of $500{' '}
+            <a
+              href="https://blockscout.com/xdai/mainnet/tokens/0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hiringboard-link"
+            >
+              wxDai
+            </a>
+            {' '}(on the{' '}
+            <a
+              href="https://www.xdaichain.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hiringboard-link"
+            >
+              xDai chain
+            </a>
+            ). 
           </motion.p>
-          <div id='consultation-button-container'>
-            <button
-              className='consultation-button'
-              initial={{ x: '100vw' }}
-              animate={{ x: 0 }}
-              transition={{ delay: 1.3 }}
-              onClick={() => {
-                console.log('Link to raid');
-              }}
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.8, duration: 0.5 }}
+          >
+            The demand for our work has been often high, so we’ve created a bidding 
+            system to prioritize consultations, for which we use{' '}
+            <a
+              href="https://rinkeby.etherscan.io/token/0x982e00b16c313e979c0947b85230907fce45d50e"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hiringboard-link"
             >
-              Get $RAID
-            </button>
-            <button
-              style={{ marginLeft: '20px' }}
-              className='consultation-button'
-              initial={{ x: '100vw' }}
-              animate={{ x: 0 }}
-              transition={{ delay: 1.3 }}
-              onClick={() => {
-                context.updateStage('next');
-              }}
-            >
-              New Consultation
-            </button>
+              $RAID
+            </a>.
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 0.5 }}
+          >
+            Adding a bid to a consultation submission is not required, but in periods 
+            of high demand it might take longer to respond to consultation requests 
+            without bids. There is a 7-day lockup period before you can withraw your 
+            bids and/or cancel the submission.
+          </motion.p>
+          <div id='hiringboard-button-container'>
+            <div>
+              <button
+                id='raid-token-button'
+                className='consultation-button'
+                initial={{ x: '100vw' }}
+                animate={{ x: 0 }}
+                transition={{ delay: 1.3 }}
+                onClick={() => {
+                  console.log('Link to raid');
+                }}
+              >
+                $RAID Token
+              </button>
+            </div>
+            <div>
+              <button
+                className='consultation-button'
+                initial={{ x: '100vw' }}
+                animate={{ x: 0 }}
+                transition={{ delay: 1.3 }}
+                onClick={() => {
+                  context.updateStage('next');
+                }}
+              >
+                New Consultation
+              </button>
+            </div>
           </div>
         </div>
         <div className="hiringboard-card">
-          <motion.h2
+          <motion.h1
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.7, duration: 0.5 }}
           >
-            Highest bids:
-          </motion.h2>
+            Open bids:
+          </motion.h1>
           {!consultations ? <p>Loading...</p> : consultations.length > 0 ? (
             <div className="bounty-list">
               {consultations.map((consultation, index) => (
                 <div onClick={() => setSelectedConsultations(consultation.project_name)} key={index} className={`bounty-list-item bounty-list-item${index % 2 !== 0 && '--2'}`}>
                   <div className="bounty-list-item-inner">
+                    <p
+                      style={{
+                        marginRight: '20px',
+                      }}
+                    >
+                      #{index < 9 ? `0${index + 1}` : index + 1}
+                    </p>
                     <p className="bounty-detail">{new Date(consultation.created).toLocaleDateString()}</p>
                     <p>
                       {consultation.airtable_id}
