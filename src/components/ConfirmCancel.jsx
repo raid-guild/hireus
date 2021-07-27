847821fcd5a9dd63182ebb37fe0e783785dcf2a7
ECWireless <Elliott@valtechcreative.com>
2021-07-27 11:01:01 -0600

    Remove fetch function from ConfirmCancel modal

--- src/components/ConfirmCancel.jsx
+++ src/components/ConfirmCancel.jsx
@@ -8,9 +8,6 @@ import {
   ModalFooter,
   Button
 } from '@chakra-ui/react';
-import { combineBids } from '../utils'
-
-import { useBids } from '../hooks/useBids';
 
 import { AppContext } from '../context/AppContext';
 
@@ -18,50 +15,14 @@ const ConfirmCancel = ({
   consultationDetails,
   setTxConfirmed,
   setShowSnackbar,
-  setConsultations,
 }) => {
-  const { contractBids } = useBids();
   const context = useContext(AppContext);
-
-  const fetchBids = async () => {
-    console.log('Fetching...');
-    try {
-      if (!contractBids) {
-        return;
-      }
-      fetch(`https://guild-keeper.herokuapp.com/hireus-v2/awaiting-raids`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "key":process.env.REACT_APP_ACCESS_KEY
-      })
-    })
-      .then((res) => res.json())
-      .then(async (data) => {
-        if (!contractBids) return;
-        const combinedBids = await combineBids(data, contractBids);
-        combinedBids.sort(function(a,b){
-          return new Date(b.created) - new Date(a.created);
-        });
-        combinedBids.sort((a,b) => Number(b.amount)-Number(a.amount));
-        setConsultations(combinedBids);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  }
 
   const onCancelAndUpdate = async (id) => {
     context.updateCancelModalStatus(false);
     setTxConfirmed(false);
     setShowSnackbar(true);
     await context.onCancel(id);
-    await fetchBids();
     setTxConfirmed(true);
   }
 
