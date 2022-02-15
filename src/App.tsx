8734c07c086fabd07f86329447fa8b0427ae8e9a
ECWireless <Elliott@valtechcreative.com>
2022-02-15 07:20:07 -0700

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import FAQ from 'components/Faq';
 import { Footer } from 'components/Footer';
+import { Header } from 'components/Header';
 import { useEffect, useState } from 'react';
 import { Router } from 'react-router-dom';
 import Routes from 'Routes';
 import history from 'utils/history';
 
 const App: React.FC = () => {
-  const [, setWindowWidth] = useState(window.innerWidth);
   const [isFaqOpen, setIsFaqOpen] = useState(false);
 
+  const [windowWidth, setWindowWidth] = useState(0);
+
   useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    window.removeEventListener('resize', () => null);
     window.addEventListener('resize', e => {
       setWindowWidth(window.innerWidth);
     });
   }, []);
 
   return (
-    <Flex height="100%" width="100%" direction="column">
+    <Flex
+      width="100vw"
+      minHeight="100vh"
+      direction="column"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Box px={{ base: '2rem', lg: '5rem' }} w="100%">
+        <Header windowWidth={windowWidth} />
+      </Box>
       <Router history={history}>
         <Routes />
       </Router>
