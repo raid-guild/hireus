c6c5b7305369112cd7a1bbc0bd0ea308808085d1
ECWireless <Elliott@valtechcreative.com>
2022-02-15 07:51:52 -0700

    Add Explainer view

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ ReactDOM.render(
     <ChakraProvider theme={theme}>
       <WalletProvider>
         <Flex
-          py="1rem"
+          pt="1rem"
           overflowX="hidden"
           background="radial-gradient(97.27% 170.54% at 98.7% 2.73%, #24003A 0%, rgba(0, 0, 0, 0) 100%),
         radial-gradient(100% 350.19% at 100% 100%, #170011 0%, rgba(23, 0, 17, 0) 100%),
