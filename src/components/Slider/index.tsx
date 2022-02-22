be095e1628756caf15e2bd2b784153e40b8c81d9
ECWireless <Elliott@valtechcreative.com>
2022-02-22 11:27:07 -0700

    Convert Slider to styled-components

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,23 +1,91 @@
-import './slider.scss';
-
+import { Flex } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import React from 'react';
+import { StyledBodyText } from 'themes/styled';
+import { theme } from 'themes/theme';
 
-type ISlider = {
+type SliderProps = {
   label: string;
   setToggleState: React.Dispatch<React.SetStateAction<boolean>>;
   toggleState: boolean;
 };
 
-const Slider: React.FC<ISlider> = ({ label, setToggleState, toggleState }) => {
+const Slider: React.FC<SliderProps> = ({
+  label,
+  setToggleState,
+  toggleState,
+}) => {
   return (
-    <div id="switch-container">
-      <p>{label}</p>
-      <label className="switch">
+    <StyledSwitchContainer align={'center'}>
+      <StyledBodyText fontSize={'14px'} textTransform={'uppercase'}>
+        {label}
+      </StyledBodyText>
+      <StyledSwitch>
         <input type="checkbox" onChange={() => setToggleState(!toggleState)} />
         <span className="slider round"></span>
-      </label>
-    </div>
+      </StyledSwitch>
+    </StyledSwitchContainer>
   );
 };
 
 export default Slider;
+
+const StyledSwitchContainer = styled(Flex)`
+  background: #111111;
+  border-radius: 4px;
+  gap: 12px;
+  padding: 8px 12px;
+`;
+
+const StyledSwitch = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 28px;
+
+  input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  span {
+    border-radius: 34px;
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #666666;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  span:before {
+    border-radius: 50%;
+    position: absolute;
+    content: '';
+    height: 20px;
+    width: 20px;
+    left: 4px;
+    bottom: 4px;
+    background-color: #c4c4c4;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  input:checked + span {
+    background-color: ${theme.colors.red};
+  }
+
+  input:focus + span {
+    box-shadow: 0 0 1px ${theme.colors.red};
+  }
+
+  input:checked + span:before {
+    -webkit-transform: translateX(20px);
+    -ms-transform: translateX(20px);
+    transform: translateX(20px);
+  }
+`;
